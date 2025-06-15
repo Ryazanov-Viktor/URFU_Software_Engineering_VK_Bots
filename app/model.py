ac19b702b02b84053385b0e084934fc1f52da537
Ryazanov-Viktor <ryazanov.v.v.2001@gmail.com>
2025-06-15 05:05:04 +0500

    Fix: use df.values to avoid feature names warning

--- app/model.py
+++ app/model.py
@@ -5,6 +5,6 @@ model = joblib.load("model/model.pkl")
 
 # Предсказываем класс и вероятность
 def make_prediction(df):
-    prediction = model.predict(df)[0]
-    probability = model.predict_proba(df)[:, 1][0]
+    prediction = model.predict(df.values)[0]
+    probability = model.predict_proba(df.values)[:, 1][0]
     return prediction, probability
